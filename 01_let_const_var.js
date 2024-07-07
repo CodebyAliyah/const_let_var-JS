af213c5f787468b18b2dbf8dd15fa0376fbe56ee
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-07 10:58:01 -0700

    Assignment Successfully

---
+++ 01_let_const_var.js
@@ -0,0 +1,18 @@
+
+const AccountId = 1234 ;
+
+let userName = "Ali Yahya" ;
+
+var userEmail = "hc@ch.com " ;
+
+console.log([AccountId , userName , userEmail]);
+
+/* 
+I will recommended you only use const & let.
+ */
+
+
+/*
+prefer not to use var beacuse of issue in block scope
+and functional scope
+*/
